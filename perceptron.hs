90a22aafdf92dba84cc41fb631029deaea5f9ae7
woodrock <j.r.h.wood98@gmail.com>
2021-08-26 00:30:21 +1200

    Type alias for Input,Output

--- perceptron.hs
+++ perceptron.hs
@@ -1,11 +1,21 @@
+import System.Random (randomR, getStdRandom) 
+import Control.Monad (replicateM)
+import Data.List (group, sort, sortBy)
+import Data.Ord (comparing)
+
 type Neuron = [Double]
 type Layer = [Neuron]
+type Input = [[Double]]
+type Output = [Double]
 
-initNeuron :: Int -> Neuron 
-initNeuron n = [ 0.01 | _ <- [0..n] ] 
+initNeuron :: Int -> IO Neuron 
+initNeuron n = do
+  let interval = randomR (-0.5, 0.5)
+  (replicateM n (getStdRandom interval)) 
 
-initLayer :: Int -> Int -> Layer 
-initLayer n m = [ initNeuron n | _ <- [0..n]]
+initLayer :: Int -> Int -> IO Layer 
+initLayer n m = do 
+  (replicateM m (initNeuron n))
 
 recall :: Neuron -> [Double] -> Double 
 recall n = threshold . neuronSum n
@@ -22,49 +32,53 @@ threshold x
 neuronSum :: Neuron -> [Double] -> Double
 neuronSum n is = sum $ zipWith (*) n ((-1):is)
 
+weightUpdate :: Double -> Double -> Double -> Double -> Double 
+weightUpdate eta diff w x = w - eta * diff * x
+
 trainOne :: Double -> [Double] -> Double -> Neuron -> Neuron 
 trainOne eta xs t ws = zipWith (weightUpdate eta diff) ws ((-1):xs)
   where diff = recall ws xs - t 
-
-weightUpdate :: Double -> Double -> Double -> Double -> Double 
-weightUpdate eta diff w x = w - eta * diff * x
 
 trainOneLayer :: Double -> [Double] -> Double -> Layer -> Layer 
 trainOneLayer _ _ _ [] = []
 trainOneLayer eta xs ts (l:ls) = trainOne eta xs ts l : trainOneLayer eta xs ts ls 
 
-trainSet :: Double -> [[Double]] -> [Double] -> Layer -> Layer 
+trainSet :: Double -> Input -> Output -> Layer -> Layer 
 trainSet _ [] _ n = n 
 trainSet eta (v:vs) (t:ts) l = trainSet eta vs ts $ trainOneLayer eta v t l
 
-train :: Int -> Double -> [[Double]] -> [Double] -> Layer -> Layer
+train :: Int -> Double -> Input -> Output -> Layer -> Layer
 train 0 _ _ _ n = n 
 train epoch eta vs ts n = train (epoch - 1) eta vs ts $ trainSet eta vs ts n 
 
-x :: [[Double]]
+x :: Input
 x = [[x,y,z] | x <- [0..n], y <- [0..n], z <- [0..n]]
   where n = 5
 
-y :: [Double]
+y :: Output
 y = map objective x 
 
 objective :: [Double] -> Double 
 objective [x,y,z] = threshold ((x - 1.0)^4 + (y - 1.0)^5 - z - 1.0)
 
-predict :: Layer -> [[Double]] -> [Double] 
-predict model = map (!!0) . map (recallLayer model) 
+mode :: [Double] -> Double 
+mode = head . head . sortBy (flip $ comparing length) . group . sort 
 
-accuracy :: [Double] -> [Double] -> Double 
+predict :: Layer -> Input -> Output 
+predict model = map mode . map (recallLayer model) 
+
+accuracy :: Output -> Output -> Double 
 accuracy y y' = (n - error) / n
   where 
     error = sum $ zipWith (\y y' -> abs (y - y')) y y'
     n = fromIntegral $ length y 
 
 main :: IO () 
-main = putStrLn $ acc 
-  where
-    acc = "Acc: " ++ show (accuracy y y_pred)
-    y_pred = predict model x  
-    model = train epochs eta x y (initLayer 3 3) 
-    eta = 0.01  
-    epochs = 50
+main = do  
+  let eta = 0.01  
+  let epochs = 50
+  layer <- initLayer 3 3 
+  let model = train epochs eta x y layer
+  let y_pred = predict model x
+  let acc = "Acc: " ++ show (accuracy y y_pred)
+  print acc
